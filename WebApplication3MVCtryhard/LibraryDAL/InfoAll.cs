1459db331a363be98458fee6a3c486d933928e55
vlad <vlad.krivonos.job@mail.ru>
2016-04-13 22:55:56 +0300

    add solution

---
+++ WebApplication3MVCtryhard/LibraryDAL/InfoAll.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using System.Data;
+using System.Configuration;
+
+namespace LibraryDAL
+{
+    public class InfoAll
+    {
+        public static DALBooksFromInfoAll[] GetInfo()
+        {
+            // NpgsqlConnection dbconn = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["HomeLibrary"].ConnectionString);
+            NpgsqlConnection dbconn = new NpgsqlConnection("Server=127.0.0.1;User Id=postgres;Password=12345;Port=5433;Database=Home Library;");
+            dbconn.Open();
+            NpgsqlCommand com; // = new NpgsqlCommand("SELECT * FROM booksinlibrarycolumns", dbconn);
+            NpgsqlDataReader reader;
+            // reader = com.ExecuteReader();
+            //DataTable dtAll = new DataTable();
+            //while (reader.Read())
+            //{
+            //    dtAll.Columns.Add(reader.GetString(0));
+            //}
+            //reader.Close();
+
+            //            string first = reader.GetString(0);
+            com = new NpgsqlCommand("SELECT Count(*) FROM booksinlibrary", dbconn);
+            int iteratormax = Convert.ToInt32(com.ExecuteScalar());
+            
+            com = new NpgsqlCommand("SELECT * FROM booksinlibrary", dbconn);
+            DALBooksFromInfoAll[] books = new DALBooksFromInfoAll[iteratormax];
+            reader = com.ExecuteReader();
+            int iterator = 0;
+            while (iterator < iteratormax)
+            {
+                reader.Read();
+                object[] newrow = new object[reader.FieldCount];
+                for (int i = 0; i < reader.FieldCount; i++)
+                    newrow[i] = reader.GetString(i);
+                books[iterator] = new DALBooksFromInfoAll(newrow);
+                //dtAll.Rows.Add(newrow);
+                iterator++;
+            }
+            reader.Close();
+
+            //GridView1.DataSource = dtAll;
+            //GridView1.DataBind();
+            //reader.Close();
+            dbconn.Close();
+
+            return books;
+        }
+    }
+}
---
+++ WebApplication3MVCtryhard/WebApplication3MVCtryhard/Controllers/HomeController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3MVCtryhard.Models;
+using Npgsql;
+using LibraryBL;
+
+namespace WebApplication3MVCtryhard.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        //
+        // GET: /Home/
+        //BookContext db = new BookContext();
+
+        public ActionResult Index()
+        {
+            //NpgsqlConnection dbconn = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["HomeLibrary"].ConnectionString);
+            //dbconn.Open();
+            //NpgsqlCommand com = new NpgsqlCommand("SELECT * FROM booksinlibrarycolumns", dbconn);
+            //NpgsqlDataReader reader;
+            //reader = com.ExecuteReader();
+            //DataTable dtAll = new DataTable();
+            //while (reader.Read())
+            //{
+            //    dtAll.Columns.Add(reader.GetString(0));
+            //}
+            //reader.Close();
+
+            ////            string first = reader.GetString(0);
+            //com = new NpgsqlCommand("SELECT * FROM booksinlibrary", dbconn);
+            //reader = com.ExecuteReader();
+            //while (reader.Read())
+            //{
+            //    object[] newrow = new object[reader.FieldCount];
+            //    for (int i = 0; i < reader.FieldCount; i++)
+            //        newrow[i] = reader.GetString(i);
+            //    dtAll.Rows.Add(newrow);
+            //}
+            //reader.Close();
+
+            ////GridView1.DataSource = dtAll;
+            ////GridView1.DataBind();
+            //reader.Close();
+            //dbconn.Close();
+
+            //return View(dtAll);
+            List<BLBooksFromInfoAll> BLBooks =  HomeControllerManager.GetData();
+            List<UIBooksFromInfoAll> UIBooks = new List<UIBooksFromInfoAll>();
+            foreach (BLBooksFromInfoAll b in BLBooks)
+            {
+                UIBooks.Add(new UIBooksFromInfoAll(b));
+            }
+            DataTable dtAll = new DataTable();
+            string[] columns = UIBooks[0].GetColumns();
+            foreach(string col in columns)
+                dtAll.Columns.Add(col);
+            foreach(UIBooksFromInfoAll b in UIBooks)
+                dtAll.Rows.Add(b.GetValues());
+            return View(dtAll);
+        }
+
+        public ActionResult AddRemoveBooks()
+        {
+            return View();
+        }
+
+        public ActionResult EditSingleBook()
+        {
+            //NpgsqlConnection dbconn = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["HomeLibrary"].ConnectionString);
+            //dbconn.Open();
+            //NpgsqlCommand com = new NpgsqlCommand("SELECT * FROM booksinlibrarycolumns", dbconn);
+            //NpgsqlDataReader reader;
+            //reader = com.ExecuteReader();
+            DataTable dtAll = new DataTable();
+            //dtAll.TableName = "Book";
+            //while (reader.Read())
+            //{
+            //    dtAll.Columns.Add(reader.GetString(0));
+            //}
+            //reader.Close();
+
+            ////            string first = reader.GetString(0);
+            //com = new NpgsqlCommand("SELECT * FROM booksinlibrary WHERE b_id=" + Request.QueryString["bid"], dbconn);
+            //reader = com.ExecuteReader();
+            //while (reader.Read())
+            //{
+            //    object[] newrow = new object[reader.FieldCount];
+            //    for (int i = 0; i < reader.FieldCount; i++)
+            //        newrow[i] = reader.GetString(i);
+            //    dtAll.Rows.Add(newrow);
+            //}
+            //reader.Close();
+            ////GridView1.DataSource = dtAll;
+            ////GridView1.DataBind();
+            //reader.Close();
+            //dbconn.Close();
+
+            //DataSet ds = new DataSet();
+            //ds.Tables.Add(dtAll);
+            //return View(ds);
+            return View();
+        }
+
+        [HttpPost]
+        public FileStreamResult SaveChanges(string bid)
+        {
+            string str = bid;
+            str += "10";
+            return null;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            //db.Dispose();
+
+            base.Dispose(disposing);
+        }
+    }
+}
